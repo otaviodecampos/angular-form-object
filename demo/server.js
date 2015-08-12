d4c917345ff7676183ae9d4fc04ddd0c0846333d
otaviodecampos <otaviodecampos@hotmail.com>
2015-08-11 23:16:12 -0300

    Update demo server

--- demo/server.js
+++ demo/server.js
@@ -1,7 +1,7 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var path = __dirname;
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 connect().use('/', serveStatic(path)).listen(port, function() {
     console.log('Web Server running on port ' + port + '...');
