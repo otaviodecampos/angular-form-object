1a1a3217badf478d2f067e56cf02e7d4a64f4e94
otaviodecampos <otaviodecampos@hotmail.com>
2015-08-11 23:42:26 -0300

    update demo/server'

--- demo/server.js
+++ demo/server.js
@@ -4,6 +4,6 @@ var path = __dirname;
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-connect().use('/', serveStatic(path)).listen(port, function() {
-    console.log('Web Server running on port ' + port + '...');
+connect().use('/', serveStatic(path)).listen(port, ip, function() {
+    console.log('Web Server running on ' + ip + ':' + port + '...');
 });
