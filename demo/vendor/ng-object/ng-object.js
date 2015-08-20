4cff9292a9b4f17dbd21da727c094cc2dd60c6b5
otaviodecampos <otaviodecampos@hotmail.com>
2015-08-20 03:03:17 -0300

    Support to add atributes and select attribute type

--- demo/vendor/ng-object/ng-object.js
+++ demo/vendor/ng-object/ng-object.js
@@ -20,7 +20,9 @@
           "array": "ngObject/ng-field-array.tpl.html",
           "string": "ngObject/ng-field-string.tpl.html",
           "boolean": "ngObject/ng-field-boolean.tpl.html",
-          "number": "ngObject/ng-field-number.tpl.html"
+          "number": "ngObject/ng-field-number.tpl.html",
+          "null": "ngObject/ng-field-null.tpl.html",
+          "undefined": "ngObject/ng-field-undefined.tpl.html"
 };
     }
 
@@ -72,6 +74,38 @@
 (function() {
 
     angular.module('ngObject')
+        .controller('NgFieldTypeCtrl', Controller);
+
+    function Controller($scope, TEMPLATE) {
+
+        this.types = Object.keys(TEMPLATE);
+        this.type = this.types[this.types.indexOf(typeof $scope.model[$scope.key])];
+
+        this.set = function(type) {
+            var value
+                , tpl = TEMPLATE[type];
+
+            if(type == 'array') {
+                value = [];
+                tpl = TEMPLATE['object'];
+            } else if(type == 'null') {
+                value = null;
+            } else if(type == 'undefined') {
+                value = undefined;
+            } else if(type == 'object') {
+                value = {};
+            } else {
+                value = window[type.charAt(0).toUpperCase() + type.slice(1)]($scope.model[$scope.key]);
+            }
+            $scope.fieldCtrl.setValue(value, tpl);
+        }
+
+    }
+
+})();
+(function() {
+
+    angular.module('ngObject')
         .controller('NgFieldCtrl', Controller);
 
     function Controller($scope, $timeout, NgObject, TEMPLATE) {
@@ -106,6 +140,13 @@
             }
         }
 
+        this.setValue = function(value, tpl) {
+            $scope.tpl = tpl || TEMPLATE[typeof value];
+            $scope.$on('$includeContentLoaded', function() {
+                $scope.model[$scope.key] = value;
+            });
+        }
+
         function isArray(value) {
             return angular.isArray(value || $scope.model);
         }
@@ -137,6 +178,24 @@
 
     function Controller($scope) {
 
+        $scope.$ngObject = {
+            array: isArray()
+        };
+
+        $scope.add = function () {
+            if(isArray()) {
+                $scope.model.push('');
+            } else {
+                var name = 'new ' + Object.keys($scope.model).length;
+                $scope.model[name] = '';
+            }
+
+        }
+
+        function isArray() {
+            return angular.isArray($scope.model);
+        }
+
     }
 
 })();
@@ -166,6 +225,21 @@
             restrict: 'E',
             templateUrl: 'ngObject/ng-field-toolbox.tpl.html',
             replace: true
+        }
+    }
+})();
+(function() {
+    'use strict'
+
+    angular.module('ngObject')
+        .directive('ngFieldTypeselect', Directive);
+
+    function Directive() {
+        return {
+            restrict: 'E',
+            templateUrl: 'ngObject/ng-field-typeselect.tpl.html',
+            replace: true,
+            controller: 'NgFieldTypeCtrl as typeCtrl'
         }
     }
 })();
@@ -257,9 +331,12 @@
 angular.module("ngObject").run(["$templateCache", function($templateCache) {$templateCache.put("ngObject/ng-field-array.tpl.html","<div class=\"form-group\">\r\n    <ng-field-toolbox></ng-field-toolbox>\r\n    <ng-field-key></ng-field-key>\r\n    <div class=\"col-sm-10\">\r\n        <p class=\"form-control-static\">array</p>\r\n    </div>\r\n</div>\r\n<div class=\"row form-group field-object\">\r\n    <div class=\"col-md-12\">\r\n        <ng-field ng-repeat=\"(i, value) in model[key] track by i\"></ng-field>\r\n    </div>\r\n</div>");
 $templateCache.put("ngObject/ng-field-boolean.tpl.html","<div class=\"ng-field-boolean form-group\">\r\n    <ng-field-toolbox></ng-field-toolbox>\r\n    <ng-field-key></ng-field-key>\r\n    <div class=\"col-sm-9\">\r\n        <label class=\"radio-inline\">\r\n            <input type=\"radio\" name=\"boolean\" ng-model=\"model[key]\" id=\"{{ ::$ngField.path }}=true\" ng-value=\"true\"> true\r\n        </label>\r\n        <label class=\"radio-inline\">\r\n            <input type=\"radio\" name=\"boolean\" ng-model=\"model[key]\" id=\"{{ ::$ngField.path }}=false\" ng-value=\"false\"> false\r\n        </label>\r\n    </div>\r\n</div>");
 $templateCache.put("ngObject/ng-field-key.tpl.html","<span class=\"ng-field-key\" ng-switch=\"$ngField[key].input && fieldKeyCtrl.allow\">\r\n    <div ng-keyup=\"fieldKeyCtrl.keyup($event)\" ng-switch-when=\"true\" class=\"col-sm-2 input-label\">\r\n        <input class=\"col-sm-2 form-control\" ng-blur=\"fieldKeyCtrl.discard()\" ng-model=\"$ngField[key].key\"  placeholder=\"string\">\r\n    </div>\r\n    <label ng-click=\"fieldKeyCtrl.input()\" ng-switch-default class=\"col-sm-2 control-label\" ng-bind=\"key\"></label>\r\n</span>");
-$templateCache.put("ngObject/ng-field-number.tpl.html","<div class=\"form-group\">\r\n    <ng-field-toolbox></ng-field-toolbox>\r\n    <ng-field-key></ng-field-key>\r\n    <div class=\"col-sm-9\">\r\n        <input class=\"form-control\" type=\"number\" ng-model=\"model[key]\" id=\"{{ ::$ngField.path }}=input\" placeholder=\"number\">\r\n    </div>\r\n</div>");
+$templateCache.put("ngObject/ng-field-null.tpl.html","<div class=\"form-group\">\r\n    <ng-field-toolbox></ng-field-toolbox>\r\n    <ng-field-key></ng-field-key>\r\n    <div class=\"col-sm-10\">\r\n        <p class=\"form-control-static\">null</p>\r\n    </div>\r\n</div>");
+$templateCache.put("ngObject/ng-field-number.tpl.html","<div class=\"form-group\">\r\n    <ng-field-toolbox></ng-field-toolbox>\r\n    <ng-field-key></ng-field-key>\r\n    <div class=\"col-sm-9\">\r\n        <div class=\"input-group\">\r\n            <input class=\"form-control\" type=\"number\" ng-model=\"model[key]\" id=\"{{ ::$ngField.path }}=input\" placeholder=\"number\">\r\n            <ng-field-typeselect></ng-field-typeselect>\r\n        </div>\r\n    </div>\r\n</div>");
 $templateCache.put("ngObject/ng-field-object.tpl.html","<div class=\"form-group\">\r\n    <ng-field-toolbox></ng-field-toolbox>\r\n    <ng-field-key></ng-field-key>\r\n    <div class=\"col-sm-10\">\r\n        <p class=\"form-control-static\">object</p>\r\n    </div>\r\n</div>\r\n<div class=\"row form-group field-object\">\r\n    <div class=\"col-md-12\">\r\n        <ng-object path=\"$ngField.path\" model=\"model[key]\"></ng-object>\r\n    </div>\r\n</div>");
-$templateCache.put("ngObject/ng-field-string.tpl.html","<div class=\"form-group\">\r\n    <ng-field-toolbox></ng-field-toolbox>\r\n    <ng-field-key></ng-field-key>\r\n    <div class=\"col-sm-9\">\r\n        <input class=\"form-control\" ng-model=\"model[key]\" id=\"{{ ::$ngField.path }}=input\" placeholder=\"string\">\r\n    </div>\r\n</div>");
+$templateCache.put("ngObject/ng-field-string.tpl.html","<div class=\"form-group\">\r\n    <ng-field-toolbox></ng-field-toolbox>\r\n    <ng-field-key></ng-field-key>\r\n    <div class=\"col-sm-9\">\r\n        <div class=\"input-group\">\r\n            <input type=\"text\" class=\"form-control\" ng-model=\"model[key]\" id=\"{{ ::$ngField.path }}=input\" placeholder=\"string\">\r\n            <ng-field-typeselect></ng-field-typeselect>\r\n        </div>\r\n    </div>\r\n</div>");
 $templateCache.put("ngObject/ng-field-toolbox.tpl.html","<div class=\"ng-field-toolbox\">\r\n    <div class=\"ng-field-toolbox-right\">\r\n        <button class=\"btn btn-default btn-xs\" ng-click=\"moveUp()\">\r\n            <span class=\"glyphicon glyphicon-arrow-up\"></span>\r\n        </button>\r\n        <button class=\"btn btn-default btn-xs\" ng-click=\"moveDown()\">\r\n            <span class=\"glyphicon glyphicon-arrow-down\"></span>\r\n        </button>\r\n        <button class=\"btn btn-default btn-xs\" ng-click=\"delete()\">\r\n            <span class=\"glyphicon glyphicon-trash\"></span>\r\n        </button>\r\n    </div>\r\n</div>");
+$templateCache.put("ngObject/ng-field-typeselect.tpl.html","<div class=\"input-group-btn\">\r\n    <button type=\"button\" class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">{{ typeCtrl.type }} <span class=\"caret\"></span></button>\r\n    <ul class=\"dropdown-menu dropdown-menu-right\">\r\n        <li ng-repeat=\"type in typeCtrl.types track by type\"><a href=\"#\" ng-click=\"typeCtrl.set(type)\">{{ type }}</a></li>\r\n    </ul>\r\n</div>");
+$templateCache.put("ngObject/ng-field-undefined.tpl.html","<div class=\"form-group\">\r\n    <ng-field-toolbox></ng-field-toolbox>\r\n    <ng-field-key></ng-field-key>\r\n    <div class=\"col-sm-10\">\r\n        <p class=\"form-control-static\">undefined</p>\r\n    </div>\r\n</div>");
 $templateCache.put("ngObject/ng-field.tpl.html","<span id=\"{{ ::$ngField.path }}\" class=\"ng-field\" ng-include=\"tpl\"></span>");
-$templateCache.put("ngObject/ng-object.tpl.html","<div class=\"row ng-object\">\r\n    <div class=\"col-md-12\">\r\n        <ng-field ng-repeat=\"(key, value) in model track by key\"></ng-field>\r\n    </div>\r\n</div>");}]);
+$templateCache.put("ngObject/ng-object.tpl.html","<div class=\"row ng-object\">\r\n    <div class=\"col-md-12\">\r\n        <ng-field ng-repeat=\"(key, value) in model track by key\"></ng-field>\r\n        <div class=\"form-group last\">\r\n            <div class=\"col-md-2\"></div>\r\n            <div class=\"col-sm-9\">\r\n                <button class=\"btn btn-hover btn-default btn-xs\" ng-click=\"add()\">\r\n                    <span class=\"glyphicon glyphicon-plus\"></span>\r\n                </button>\r\n            </div>\r\n        </div>\r\n    </div>\r\n</div>");}]);
