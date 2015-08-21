4d41909da7c1ad0ddb36e0ac07d35ffbca69ce7f
otaviodecampos <otaviodecampos@hotmail.com>
2015-08-21 09:05:34 -0300

    Fixes and updates

--- demo/source/app/controller/demo.controller.js
+++ demo/source/app/controller/demo.controller.js
@@ -6,22 +6,7 @@
 
     function Controller($scope) {
 
-        this.example = {
-            name: 'demo',
-            homepage: 'https://github.com/otaviodecampos/ng-object',
-            author: {
-                name: 'otaviodecampos',
-                email: 'otaviodecampos@hotmail.com',
-                active: true,
-                age: 25,
-                knows: [
-                    'angular', 'javascript', 'java', 'css', 'html'
-                ],
-                books: {
-                    name: 'Develop with Angular'
-                }
-            }
-        }
+        this.example = {};
 
     }
 
